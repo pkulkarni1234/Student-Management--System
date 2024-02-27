aeb50320af0d615306d0f8a142c8522c3d45aa8a
Pratiksha Santosh Kulkarni <146941215+pkulkarni1234@users.noreply.github.com>
2024-02-27 12:27:05 +0530

    update

--- studentrecordms/includes/dbconnection.php
+++ studentrecordms/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("studentmanage.mysql.database.azure.com", "student", "@Admin123", "student");
+$con=new mysqli("studentmanage.mysql.database.azure.com", "student", "@Admin123", "student");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }

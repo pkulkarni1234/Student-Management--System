f0e91fea41204e56cf93f55d8a73cf4359cc40fb
Pratiksha Santosh Kulkarni <146941215+pkulkarni1234@users.noreply.github.com>
2024-02-29 22:15:39 +0530

    update

--- studentrecordms/dashboard.php
+++ studentrecordms/dashboard.php
@@ -29,39 +29,6 @@ if (strlen($_SESSION['aid']==0)) {
 
 
 
-<style>
-  #chatbot-button {
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    z-index: 999;
-    padding: top 150px;
-  }
-
-  #open-chatbot {
-    background-color: #4CAF50;
-    color: white;
-    border: none;
-    padding: 10px;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-
-  #chatbot-frame {
-    display: none;
-    position: fixed;
-    bottom: 20px;
-    right: 20px;
-    z-index: 1000;
-  }
-
-  #chatbot-iframe {
-    border: none;
-    height: 502px;
-    max-height: 502px;
-    width: 300px; /* Adjust the width as needed */
-  }
-</style>
 
 
 </head>
@@ -288,16 +255,7 @@ $cities=mysqli_num_rows($query5);
 			
 		</div>
 		
- <!-- Chatbot Button -->
- <div id="chatbot-button">
-  <button id="open-chatbot">Chat with Us</button>
-</div>
 
-<!-- Chatbot Iframe -->
-<div id="chatbot-frame">
- <iframe src="https://webchat.botframework.com/embed/student-bot?s=kaHvISwkEfE.PX69E6jLiJ7JuS29visa7-hEAXjhXJTVwgkpBIkAo8o" style="height: 400px; max-height: 400px;"></iframe>
-</div>
-	</div>
 
        
 	
